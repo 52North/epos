edac37b71463683ab7ac8f51e3e4e850602175a4
matthesrieke <m.rieke@52north.org>
2014-01-20 15:32:48 +0100

    move some ServiceLoaded components

--- epos-api/src/main/java/org/n52/epos/transform/TransformationToEposEventRepository.java
+++ epos-api/src/main/java/org/n52/epos/transform/TransformationToEposEventRepository.java
@@ -21,7 +21,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA or
  * visit the Free Software Foundation web page, http://www.fsf.org.
  */
-package org.n52.epos.transform.xmlbeans;
+package org.n52.epos.transform;
 
 import java.util.HashSet;
 import java.util.ServiceLoader;
